35a85643442d031e898d1294de679d0c20d668d2
agiegerich <albert-giegerich@uiowa.edu>
2016-02-22 21:27:07 -0600

    Got rid of test.js reference.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,20 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() {
+    /**
+     * Set the result to be a Json result.
+     */
+    public Result jsonResult(Result response) {
+        response().setContentType("application/json; charset=utf-8");   
+        return response;
+    }
+
+    public Result index() { 
         return ok(index.render("Your new application is ready."));
+    }
+
+    public Result registerUser() {
+        return null;
     }
 
 }
