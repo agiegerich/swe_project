9b3aae781b885a79cc932b3e91baec707c37c67b
agiegerich <albert-giegerich@uiowa.edu>
2016-04-26 15:01:13 -0500

    Fixed bug in cart.

--- app/controllers/ProductController.java
+++ app/controllers/ProductController.java
@@ -32,7 +32,7 @@ public class ProductController extends Controller {
             session().clear();
             return Application.sendBadRequest("Invalid Session: User with email " + email + "does not exist.");
         }
-        return ok( product.render(user.get(), Product.findAll(), R.categories, productForm) );
+        return ok(product.render(user.get(), Product.findAll(), R.categories, productForm));
     }
 
     public Result history() {
@@ -51,7 +51,7 @@ public class ProductController extends Controller {
                 item.save();
             }
         }
-        return ok( shoppingHistory.render(user.get().shoppingHistory(), "0", "") );
+        return ok(shoppingHistory.render(user.get().shoppingHistory(), "0", ""));
     }
 
     public Result replace(String emailAdr, Long id) {
@@ -64,7 +64,7 @@ public class ProductController extends Controller {
         email.setBodyText("item.product.name\n item.product.category\n item.quantityInCart\n item.product.quantity\n");
 
         // Send the email.
-        MailerPlugin.send( email );
+        MailerPlugin.send(email);
 
         return redirect(routes.ProductController.history());
     }
@@ -151,10 +151,23 @@ public class ProductController extends Controller {
             User user = optUser.get();
             CartItem newItem = null;
             // Check if the item is already in the cart
-            Product product = potentialProduct.get();
-            CartItem cartItem = new CartItem(product, productQuantity);
-            user.getShoppingCart().add(cartItem);
-            user.save();
+            for (CartItem cartItem : user.getShoppingCart()) {
+                if ( cartItem.getProduct().getId() == potentialProduct.get().getId() && !cartItem.done) {
+                    newItem = cartItem;
+                }
+            }
+
+            if (newItem != null ){
+                newItem.quantityInCart += productQuantity;
+                newItem.save();
+
+            } else {
+
+                Product product = potentialProduct.get();
+                CartItem cartItem = new CartItem(product, productQuantity);
+                user.getShoppingCart().add(cartItem);
+                user.save();
+            }
         }
         return ok(Json.toJson(jsonObject));
     }
